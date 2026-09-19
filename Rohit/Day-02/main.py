9b18841b6feefb7de2f7c98a8bbfd21f8daeaaed
Rohit Lonkar <lonkarravi00@gmail.com>
2026-09-19 17:19:03 +0530

    checked memory allocation of string and integer

--- Rohit/Day-02/main.py
+++ Rohit/Day-02/main.py
@@ -1,4 +1,5 @@
-import sys
+# import sys
 
-print(sys.getsizeof(6))
-print(sys.getsizeof(""))
+# print(f"Integer Takes {sys.getsizeof(0)} bytes")
+# print(f"Empty String Takes {sys.getsizeof("")} bytes")
+
