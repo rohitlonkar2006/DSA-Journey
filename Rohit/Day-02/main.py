89d9dcf9624e3106c517e8b1d309962b2a52502f
Rohit Lonkar <lonkarravi00@gmail.com>
2026-09-19 17:30:31 +0530

    checked for set of strings

--- Rohit/Day-02/main.py
+++ Rohit/Day-02/main.py
@@ -1,10 +1,18 @@
 import sys
 
 # print(f"Integer Takes {sys.getsizeof(0)} bytes")
+
 # print(f"Empty String Takes {sys.getsizeof("")} bytes")
 
-print(f"Floating Takes {sys.getsizeof(5.0)} bytes")
-print(f"Boolean Takes {sys.getsizeof(True)} bytes")
-print(f"Boolean Takes {sys.getsizeof(False)} bytes")
+# print(f"Floating Takes {sys.getsizeof(5.0)} bytes")
 
-print(f"None Takes {sys.getsizeof(None)} bytes")
+# print(f"Boolean Takes {sys.getsizeof(True)} bytes")
+# print(f"Boolean Takes {sys.getsizeof(False)} bytes")
+
+# print(f"None Takes {sys.getsizeof(None)} bytes")
+
+strings = ["","a","Hello","Hi,I am rohit!"]
+for s in strings:
+    display = s if len(s)<=40 else s[:37] + "..."
+    print(f"'{display}'")
+    print(f"Length {len(s)} characters, Size: {sys.getsizeof(s)} bytes")
