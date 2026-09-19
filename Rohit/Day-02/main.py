054a8880dbeb207ea9f7bee1d2b9df7b5bac6be5
Rohit Lonkar <lonkarravi00@gmail.com>
2026-09-19 17:23:19 +0530

    added more data types

--- Rohit/Day-02/main.py
+++ Rohit/Day-02/main.py
@@ -1,5 +1,10 @@
-# import sys
+import sys
 
 # print(f"Integer Takes {sys.getsizeof(0)} bytes")
 # print(f"Empty String Takes {sys.getsizeof("")} bytes")
 
+print(f"Floating Takes {sys.getsizeof(5.0)} bytes")
+print(f"Boolean Takes {sys.getsizeof(True)} bytes")
+print(f"Boolean Takes {sys.getsizeof(False)} bytes")
+
+print(f"None Takes {sys.getsizeof(None)} bytes")
