fde7c6dabe802aa533549c52ab7917e364aea1be
Rohit Lonkar <lonkarravi00@gmail.com>
2026-09-21 21:10:49 +0530

    its buggy

--- Rohit/Day-03/main.py
+++ Rohit/Day-03/main.py
@@ -1,3 +1,5 @@
+import traceback
+
 # def multiply(a,b):
 #     return a*b
 
@@ -16,8 +18,15 @@
 
 def process_order(order_id):
     print(f">> Processing Order #{order_id}")
-    print()
+    traceback.print(order_id)
 
 def validate_order(order_id):
     print(f">> Validating Order #{order_id}")
-    process_order()
+    recieve(order_id)
+
+def recieve(order_id):
+    print(f"Recieving order #{order_id}")
+    validate_order(order_id)
+
+recieve(67)
+print("Finished")
