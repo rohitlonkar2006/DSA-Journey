6b51b4f1c3c6f6df6556e0dbe708f72376cadcd4
Rohit Lonkar <lonkarravi00@gmail.com>
2026-09-26 19:51:54 +0530

    done

--- Rohit/Day-04/main.py
+++ Rohit/Day-04/main.py
@@ -1,2 +1,15 @@
 def second_largest(nums):
     first = 0
+    second = 0
+    
+    for x in nums:
+        if x > first:
+            second = first
+            first = x
+        elif x > second and x != first:
+            second = x
+            
+    return second
+
+obj = nums([1,2,3,4,5,3])
+print("Second Largest:",obj)
